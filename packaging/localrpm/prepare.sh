abb7bd46d30388f62ed3b5b8200ba90dfc3ac86b
DEEPAK BHONSLE <dbhonsle@rewdale.com>
2021-05-16 08:59:23 +0530

    localrpm build support WIP-8

--- packaging/localrpm/prepare.sh
+++ packaging/localrpm/prepare.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 #COMMIT_SHORT=$(git rev-parse --short=8 HEAD)
-VERSION := $(shell cat ../../VERSION)
+VERSION := $(cat ./VERSION)
 
 mkdir -p build/
 #git archive --prefix "podman-${COMMIT_SHORT}/" --format "tar.gz" HEAD -o "build/podman-${COMMIT_SHORT}.tar.gz"
